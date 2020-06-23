0f926d2477ae62c8d46ed8b2bda66827245b5c92
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-23 00:04:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,8 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  cat_hash = {:cat => "chonk", :cat_two => "little cat"}
+  cat_hash
 end
 
 def pioneer
